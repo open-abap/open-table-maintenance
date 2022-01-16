8a4aa043fbada4423fb4c750ecab8890b1cda8f3
Lars Hvam <larshp@hotmail.com>
2022-01-16 12:45:54 +0100

    wait 2 seconds

--- test/puppeteer.js
+++ test/puppeteer.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('http://localhost:3000/abap');
-  await page.waitForNavigation({waitUntil: 'networkidle2'});
+  await page.waitForTimeout(2000);
   await page.screenshot({ path: 'screenshot.png' });
   await browser.close();
 })();
