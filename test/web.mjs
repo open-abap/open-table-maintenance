55030e415154a75dc0ae1e3b7b6f107441ebf79b
Lars Hvam <larshp@hotmail.com>
2022-06-12 15:03:36 +0200

    update

--- test/web.mjs
+++ test/web.mjs
@@ -1,8 +1,53 @@
-async function run() {
-  const init = await import("../output/init.mjs");
-  await init.initializeABAP();
-  const shim = await import("../output/cl_express_icf_shim.clas.mjs");
+const init = await import("../output/init.mjs");
+await init.initializeABAP();
+const shim = await import("../output/cl_express_icf_shim.clas.mjs");
 
+async function redirectFetch(url, options) {
+  let data = "";
+
+  let res = {
+    append: (d) => {
+      console.dir("append2: " + d); },
+    send: (d) => {
+      console.dir("send2");
+      data = Buffer.from(d).toString();
+    },
+    status: (status) => {
+      console.dir("status2: " + status);
+      return res; },
+  }
+
+  let method = options?.method;
+  if (method === undefined) {
+    method = "GET";
+  }
+  let body = options?.body;
+  if (body === undefined) {
+    body = "";
+  }
+
+  return new Promise((resolve, reject) => {
+    const req = {
+      body: body,
+      method: method,
+      path: url,
+    };
+    console.dir(req);
+    shim.cl_express_icf_shim.run({req: req, res, class: "ZCL_HTTP_HANDLER"}).then(() => {
+      console.log("redirectFetch RESPONSE,");
+      console.dir(data);
+      resolve({
+        json: () => {
+          return new Promise((resolve, reject) => {
+            resolve(JSON.parse(data));
+          });
+        }
+      });
+    });
+  });
+}
+
+async function run() {
   let res = {
     append: (data) => {
       console.dir("append: " + data); },
@@ -11,13 +56,7 @@ async function run() {
       let r = Buffer.from(data).toString();
 
       document.write(r);
-
-      globalThis.fetch = (url, options) => {
-        console.dir("fetch1");
-        console.dir(url);
-        console.dir(options);
-//        shim.cl_express_icf_shim.run({req: {body: "", method: "GET", path: "/rest"}, res, class: "ZCL_HTTP_HANDLER"});
-      };
+      globalThis.fetch = redirectFetch;
 
       setTimeout(() => {
         console.dir("dispatch load");
@@ -29,7 +68,7 @@ async function run() {
       return res; },
   }
 
-  await shim.cl_express_icf_shim.run({req: {body: "", method: "GET", path: "/"}, res, class: "ZCL_HTTP_HANDLER"});
+  await shim.cl_express_icf_shim.run({req: {body: "", method: "GET", path: ""}, res, class: "ZCL_HTTP_HANDLER"});
 }
 
 run();
